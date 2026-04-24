dbad6b21d404232ab7c1505697141019013ba4d8
Amandeep <2023kucp1077@iiitkota.ac.in>
2026-04-24 19:48:52 +0530

    core: basic auth done

---
+++ src/routes/user.route.ts
@@ -0,0 +1,81 @@
+import { Hono } from "hono";
+import { PrismaClient } from "../generated/prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign } from "hono/jwt";
+
+const userRouter = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
+
+userRouter.post("/api/v1/signup", async (c) => {
+  const prisma = new PrismaClient({
+    accelerateUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        username: body.username,
+        password: body.password,
+        name: body.name,
+      },
+    });
+
+    const token = await sign(
+      {
+        userId: user.id,
+      },
+      c.env.JWT_SECRET,
+    );
+
+    return c.json({
+      meessage: "Signed Up",
+      jwtToken: token,
+    });
+  } catch (e) {
+    c.status(411);
+    return c.text("Invalid");
+  }
+});
+
+userRouter.post("/api/v1/signin", async (c) => {
+  const prisma = new PrismaClient({
+    accelerateUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        username: body.username,
+        password: body.password,
+      },
+    });
+
+    if (!user) {
+      return c.text("Forbidden", 403);
+    }
+
+    const token = await sign(
+      {
+        userId: user!.id,
+      },
+      c.env.JWT_SECRET,
+    );
+
+    return c.json({
+      message: "logged in",
+      jwtToken: token,
+    });
+  } catch (e) {
+    c.status(411);
+    return c.text("Invalid");
+  }
+});
+
+export default userRouter;
